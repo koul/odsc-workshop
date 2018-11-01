6e866b7e9e9edac399589d17fe0fd6052f4f3ffd
Meher Kasam <meheranandk@gmail.com>
2018-11-01 00:25:26 -0700

    Add macOS 10.13 support; flip horizontally

--- Mac/MacCameraApp/AppDelegate.swift
+++ Mac/MacCameraApp/AppDelegate.swift
@@ -3,24 +3,12 @@
 //  MacCameraApp
 //
 //  Created by Meher Kasam on 10/28/18.
-//  Copyright © 2018 Conference. All rights reserved.
+//  Copyright © 2018 Meher Kasam. All rights reserved.
 //
 
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
 
